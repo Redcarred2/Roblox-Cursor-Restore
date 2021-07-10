68bc404b1f93f5c437e2b3d4f5e2c0168e826bef
Red <57776274+Redcarred2@users.noreply.github.com>
2021-07-10 12:23:43 -0600

    Almost Ready for 1.0

---
+++ Script.bat
@@ -0,0 +1,29 @@
+@echo off
+echo "Redcarred2's Cursor restore script for roblox, if you paid for this, you got scammed."
+echo "Please report all errors to Redcarred2#6192"
+:checkzip
+if exist Cursors.zip (
+  echo "Cursors.zip exists, not downloading."
+  goto checkdir
+) else (
+  echo "Cursors.zip doesn't exist, downloading"
+  curl -L -O Cursors.zip https://github.com/Redcarred2/Roblox-Cursor-Restore/raw/main/Cursors.zip
+  goto checkdir
+)
+:checkdir
+if exist Cursors\ (
+  echo "Cursors folder exists, not unzipping"
+  goto copy
+) else (
+  echo "Cursors folder doesn't exist, unzipping"
+  powershell -command "Expand-Archive -LiteralPath Cursors.zip"
+  goto copy
+)
+:copy
+if exist %LocalAppData%\Roblox\Versions\version-b1fc630a745a4f26\content\textures\Cursors\KeyboardMouse\ (
+  del /Q %LocalAppData%\Roblox\Versions\version-b1fc630a745a4f26\content\textures\Cursors\KeyboardMouse\*.*
+  xcopy Cursors\*.pdf %LocalAppData%\Roblox\Versions\version-b1fc630a745a4f26\content\textures\Cursors\KeyboardMouse\ /s
+) else (
+  echo "An error has occured, Is Roblox Installed?"
+)
+
